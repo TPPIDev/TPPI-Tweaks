deae95d698266550405355bcd691ec4558299004
tterrag1098 <tterrag1098@gmail.com>
2013-12-23 23:56:06 -0500

    Added comment system, and comments to make sense of my spaghetti code

--- src/tppitweaks/config/ConfigurationHandler.java
+++ src/tppitweaks/config/ConfigurationHandler.java
@@ -17,10 +17,10 @@ public class ConfigurationHandler
 	public static int bookID;
 	public static String bookTitle;
 	public static String bookAuthor;
+
+	/** ArrayList of Strings, the strings are each one whole page **/
 	public static List<String> bookText;
-	
-	private static final int COMMENT_LINE_NUM = 16;
-	
+
 	public static String[] am2MobKeys = { "EntityHecate", "EntityDarkMage", "EntityWaterElemental", "EntityManaElemental", "EntityDryad", "EntityManaCreeper", "EntityDarkling" };
 
 	public static void init(File file)
@@ -40,21 +40,51 @@ public class ConfigurationHandler
 		config.save();
 	}
 
+	/**
+	 * Method that gathers the info for the book given to players on spawn
+	 * 
+	 * @param file
+	 *            - The input stream to gather the text from
+	 */
 	public static void loadBookText(InputStream file)
 	{
 		Scanner scanner;
 		scanner = new Scanner(file);
-		
+
 		bookText = new ArrayList<String>();
-		
-		for (int i = 0; i < COMMENT_LINE_NUM; i++)
-			scanner.nextLine();
-		
+
 		String nextPage = "";
 		while (scanner.hasNextLine())
 		{
 			String temp = scanner.nextLine();
+
 			System.out.println("temp:" + temp);
+
+			// If the line is a comment
+			if (temp.length() == 0 || temp.startsWith("**"))
+			{
+				// If the line is possibly a line-skip comment
+				if (temp.startsWith("***") && temp.length() == 4)
+				{
+					// Skip the requested amount of lines by parsing the number after the asterisks
+					for (int i = 0; i <= Integer.parseInt(temp.substring(3, 4)); i++)
+					{
+						System.out.println("line skipped");
+						scanner.nextLine();
+					}
+				}
+				
+				// If the line is not a valid line-skip comment, and does not have more than 3 asterisks
+				else if (temp.startsWith("***") && !temp.startsWith("****"))
+				{
+					System.out.println("TPPI - Invalid line-skip in changelog. This may not work as intended");
+				}
+				
+				// Finally, do not add this to the page
+				continue;
+			}
+
+			// If this line is a page break
 			if (temp.equals("~~~"))
 			{
 				bookText.add(nextPage);
@@ -62,20 +92,21 @@ public class ConfigurationHandler
 			}
 			else
 			{
+				// If there is a line break
 				if (temp.charAt(temp.length() - 1) == '~')
 				{
 					temp = temp.substring(0, temp.length() - 1);
 					temp += "\n";
 					nextPage += temp;
 				}
+				// In the end just concatenate to the page
 				else
 					nextPage += temp + " ";
-			}			
+			}
 		}
 
-		
 		bookText.add(nextPage);
-		
+
 		scanner.close();
 	}
 }
