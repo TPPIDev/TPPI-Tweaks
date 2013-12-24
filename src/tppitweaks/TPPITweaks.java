1a8238a38442fc79edecf82904aa56e4c3e7b39a
tterrag1098 <tterrag1098@gmail.com>
2013-12-23 22:11:01 -0500

    U MEAN LIK DIS?

--- src/tppitweaks/TPPITweaks.java
+++ src/tppitweaks/TPPITweaks.java
@@ -15,29 +15,33 @@ import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 
-@Mod(modid="TPPITweaks", name="TPPI Tweaks", version="1", dependencies="after:arsmagica2")
-@NetworkMod(channels={"tppitweaks"}, clientSideRequired=true, serverSideRequired=false)
-public class TPPITweaks {
- 
+@Mod(modid = "TPPITweaks", name = "TPPI Tweaks", version = "1", dependencies = "after:arsmagica2")
+@NetworkMod(channels = { "tppitweaks" }, clientSideRequired = true, serverSideRequired = false)
+public class TPPITweaks
+{
+
 	@Instance("TPPITweaks")
 	public static TPPITweaks instance;
- 
+
 	@EventHandler
-	public void preInit(FMLPreInitializationEvent event) {
-		
+	public void preInit(FMLPreInitializationEvent event)
+	{
+
 		ConfigurationHandler.init(event.getSuggestedConfigurationFile());
 		ConfigurationHandler.loadBookText(TPPITweaks.class.getResourceAsStream("/assets/tppitweaks/lang/TPPIChangelog.txt"));
 	}
- 
+
 	@EventHandler
-	public void init(FMLInitializationEvent event) {
+	public void init(FMLInitializationEvent event)
+	{
 		AM2SpawnControls.doAM2SpawnControls();
 		MinecraftForge.EVENT_BUS.register(new BookEventHandler());
 		TPPIBook.registerRecipes();
 	}
- 
+
 	@EventHandler
-	public void postInit(FMLPostInitializationEvent event) {
-		
+	public void postInit(FMLPostInitializationEvent event)
+	{
+
 	}
 }
