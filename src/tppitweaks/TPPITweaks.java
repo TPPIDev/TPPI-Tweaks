a7864b738395051c012d3156ce590e5ea99a895f
Garrett S-D <tterrag1098@gmail.com>
2014-01-14 16:11:35 -0500

    Revert "Don't ignore this. This is awesome."
    This reverts commit 735a12a4d870ed1a02114f7be942738087b01be5.

--- src/tppitweaks/TPPITweaks.java
+++ src/tppitweaks/TPPITweaks.java
@@ -62,7 +62,9 @@ public class TPPITweaks
 
 	@EventHandler
 	public void init(FMLInitializationEvent event)
-	{		
+	{
+		AM2SpawnControls.doAM2SpawnControls();
+		
 		eventHandler = new TPPIEventHandler();
 		MinecraftForge.EVENT_BUS.register(eventHandler);
 		ModItems.registerRecipes();
