1de43c1e6b474a361978ea95c56a6a033b4f8d72
tterrag1098 <tterrag1098@gmail.com>
2014-01-16 21:47:21 -0500

    Add coremod

---
+++ src/tppitweaks/core/CoreTPPITweaks.java
@@ -0,0 +1,45 @@
+package tppitweaks.core;
+
+import java.util.Map;
+
+import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
+
+public class CoreTPPITweaks implements IFMLLoadingPlugin{
+
+	public CoreTPPITweaks()
+	{
+		//TODO TT stuff
+	}
+	
+	@Override
+	@Deprecated
+	String[] getLibraryRequestClass() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String[] getASMTransformerClass() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getModContainerClass() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getSetupClass() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void injectData(Map<String, Object> data) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
