6f3ebc9fb14aa04c24022db9c7ea7f95b998e47e
esKaayY <esKaayY@users.noreply.github.com>
2014-01-22 02:19:04 -0500

    Update TT references to something more permanent.

--- src/tppitweaks/lib/Reference.java
+++ src/tppitweaks/lib/Reference.java
@@ -7,8 +7,8 @@ public class Reference
 	public static final String CHANNEL = "tppitweaks";
 	public static final String TAB_NAME = "tabTPPI";
 	public static final String THAUMCRAFT_FILENAME = "Thaumcraft4.0.5b.zip";
-	public static String TT_FILENAME = "ThaumicTinkerer 2.1-65.jar";
-	public static String TT_KAMI_FILENAME = "ThaumicTinkererKAMI_j6.jar";
+	public static String TT_FILENAME = "ThaumicTinkerer.jar";
+	public static String TT_KAMI_FILENAME = "ThaumicTinkererKAMI.jar";
 	public static File modsFolder;
 	
 	public static final String DEPENDENCIES = 
