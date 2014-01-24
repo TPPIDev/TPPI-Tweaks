75e7f38ca872dd48e03c131de170f71a0f149afe
Garrett S-D <tterrag1098@gmail.com>
2014-01-23 20:29:18 -0500

    How did I go back a version? o.O

--- src/tppitweaks/TPPITweaks.java
+++ src/tppitweaks/TPPITweaks.java
@@ -29,7 +29,7 @@ import cpw.mods.fml.common.registry.GameRegistry;
 @NetworkMod(serverSideRequired = true, clientSideRequired = true, channels = { Reference.CHANNEL }, packetHandler = PacketHandler.class)
 public class TPPITweaks {
 
-	public static final String VERSION = "0.6.2d";
+	public static final String VERSION = "0.6.3d";
 
 	@Instance("TPPITweaks")
 	public static TPPITweaks instance;
