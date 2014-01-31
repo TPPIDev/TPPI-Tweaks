e8412e95b6c73b73c62189f20f2cac26b8b2f1fa
tterrag1098 <tterrag1098@gmail.com>
2014-01-31 02:39:30 -0500

    Version bump for jenkins test

--- src/tppitweaks/TPPITweaks.java
+++ src/tppitweaks/TPPITweaks.java
@@ -33,7 +33,7 @@ import cpw.mods.fml.common.registry.GameRegistry;
 @NetworkMod(serverSideRequired = true, clientSideRequired = true, channels = { Reference.CHANNEL }, packetHandler = PacketHandler.class)
 public class TPPITweaks {
 
-	public static final String VERSION = "0.8.0-alpha2";
+	public static final String VERSION = "0.8.0-alpha3";
 
 	@Instance("TPPITweaks")
 	public static TPPITweaks instance;
