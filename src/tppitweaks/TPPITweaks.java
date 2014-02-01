ce3f64d10bd4d8d81325ff81825664b2723e6006
tterrag1098 <tterrag1098@gmail.com>
2014-02-01 06:11:36 -0500

    Version bump.

--- src/tppitweaks/TPPITweaks.java
+++ src/tppitweaks/TPPITweaks.java
@@ -34,7 +34,7 @@ import cpw.mods.fml.common.registry.GameRegistry;
 @NetworkMod(serverSideRequired = true, clientSideRequired = true, channels = { Reference.CHANNEL }, packetHandler = PacketHandler.class)
 public class TPPITweaks {
 
-	public static final String VERSION = "0.8.0-alpha4a";
+	public static final String VERSION = "0.8.0-alpha4b";
 
 	@Instance("TPPITweaks")
 	public static TPPITweaks instance;
