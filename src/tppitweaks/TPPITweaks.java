9438c4de1252bb33c697f68c70e24a81902a7482
tterrag1098 <tterrag1098@gmail.com>
2014-02-02 05:15:59 -0500

    Update welcome packet to contain relevant info for 0.2.0 Fix localization of Disassembler Core

--- src/tppitweaks/TPPITweaks.java
+++ src/tppitweaks/TPPITweaks.java
@@ -34,7 +34,7 @@ import cpw.mods.fml.common.registry.GameRegistry;
 @NetworkMod(serverSideRequired = true, clientSideRequired = true, channels = { Reference.CHANNEL }, packetHandler = PacketHandler.class)
 public class TPPITweaks {
 
-	public static final String VERSION = "0.8.0-beta1d";
+	public static final String VERSION = "0.8.0-beta1e";
 
 	@Instance("TPPITweaks")
 	public static TPPITweaks instance;
