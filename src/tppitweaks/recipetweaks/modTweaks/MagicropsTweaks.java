32916033d0740685cd10cf429c525e29b628e276
tterrag1098 <tterrag1098@gmail.com>
2014-02-06 15:40:44 -0500

    Remove XP seeds

--- src/tppitweaks/recipetweaks/modTweaks/MagicropsTweaks.java
+++ src/tppitweaks/recipetweaks/modTweaks/MagicropsTweaks.java
@@ -5,6 +5,11 @@ import net.minecraftforge.oredict.OreDictionary;
 
 public class MagicropsTweaks
 {
+	public static void init()
+	{
+		TweakerBase.markItemForRecipeRemoval(magicalcrops.mod_mCrops.mSeedsXP.itemID, -1);
+	}
+	
 	public static void registerOres()
 	{
 		OreDictionary.registerOre("oreMCropsEssence", new ItemStack(magicalcrops.mod_mCrops.BlockOreEssence));
