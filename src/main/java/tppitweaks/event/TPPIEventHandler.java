d4f735be6ba7415814bacc9b66ea8765681ff67c
tterrag1098 <tterrag1098@gmail.com>
2014-06-18 13:41:23 -0400

    Splash texts because why not

--- src/main/java/tppitweaks/event/TPPIEventHandler.java
+++ src/main/java/tppitweaks/event/TPPIEventHandler.java
@@ -1,6 +1,9 @@
 package tppitweaks.event;
 
+import java.util.Random;
+
 import net.minecraft.client.gui.GuiMainMenu;
+import net.minecraft.util.StatCollector;
 import net.minecraftforge.client.event.GuiOpenEvent;
 import net.minecraftforge.event.ForgeSubscribe;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
@@ -9,6 +12,7 @@ import tppitweaks.client.gui.MaricultureGui;
 import tppitweaks.config.ConfigurationHandler;
 import tppitweaks.recipetweaks.modTweaks.DATweaks;
 import cpw.mods.fml.common.Loader;
+import cpw.mods.fml.common.ObfuscationReflectionHelper;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
@@ -32,6 +36,10 @@ public class TPPIEventHandler
 				event.gui = new IRCGui();
 				shouldLoadGUI = false;
 			}
+			else
+			{
+				ObfuscationReflectionHelper.setPrivateValue(GuiMainMenu.class, (GuiMainMenu) event.gui, getRandTPPISplash(), "splashText", "field_73975_c");
+			}
 		}
 	}
 	
@@ -42,5 +50,23 @@ public class TPPIEventHandler
 		{
 			DATweaks.addTooltip(event);
 		}
+	}
+	
+	/**
+	 * Automatically figures out the number of splash text lines in the lang file, returns a random one.
+	 */
+	private String getRandTPPISplash()
+	{
+		int max = 1;
+		String base = "tppi.splash.text.";
+		while (!StatCollector.translateToLocal(base + max).equals(base + max))
+		{
+			max++;
+		}
+		max--;
+		
+		int rand = new Random().nextInt(max) + 1;
+		
+		return StatCollector.translateToLocal("tppi.splash.text." + rand);
 	}
 }
