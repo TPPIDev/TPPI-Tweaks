4fb52872f860dc265e10e3ba14b2a9dfd6c1795a
tterrag1098 <tterrag1098@gmail.com>
2014-09-10 16:34:38 -0400

    Fix AM2.1 Keystone Receptacle bug

--- src/main/java/tppitweaks/core/CoreMethods.java
+++ src/main/java/tppitweaks/core/CoreMethods.java
@@ -1,5 +1,6 @@
 package tppitweaks.core;
 
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntityFurnace;
@@ -14,6 +15,11 @@ public class CoreMethods
 	public static int getFuelBurnSurvivalist(ItemStack item)
 	{
 		return getFurnaceBurnTime(item) / 200;
+	}
+	
+	public ItemStack transferStackInSlot(EntityPlayer player, int slot)
+	{
+		return null; // screw it
 	}
 
 	public static int getFurnaceBurnTime(ItemStack item)
--- src/main/java/tppitweaks/core/CoreTransformer.java
+++ src/main/java/tppitweaks/core/CoreTransformer.java
@@ -1,5 +1,7 @@
 package tppitweaks.core;
 
+import static org.objectweb.asm.Opcodes.*;
+
 import java.util.Iterator;
 
 import net.minecraft.launchwrapper.IClassTransformer;
@@ -7,23 +9,26 @@ import net.minecraft.launchwrapper.IClassTransformer;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.Label;
-import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.tree.AbstractInsnNode;
 import org.objectweb.asm.tree.ClassNode;
 import org.objectweb.asm.tree.InsnList;
+import org.objectweb.asm.tree.InsnNode;
 import org.objectweb.asm.tree.LabelNode;
 import org.objectweb.asm.tree.MethodInsnNode;
 import org.objectweb.asm.tree.MethodNode;
 import org.objectweb.asm.tree.VarInsnNode;
 
+import tppitweaks.TPPITweaks;
+
 public class CoreTransformer implements IClassTransformer
 {
 	private final String FURNACE_GEN_CLASS = "extrautils.tileentity.generator.TileEntityGeneratorFurnace";
 	private final String SURVIVALIST_GEN_CLASS = "extrautils.tileentity.generator.TileEntityGeneratorFurnaceSurvival";
-	
+	private final String KEYSTONE_CONTAINER_CLASS = "am2.containers.ContainerKeystoneLockable";
+
 	private final String FURNACE_GEN_METHOD = "getFuelBurn";
 	private final String FURNACE_GEN_METHOD_DESC = "(Lnet/minecraft/item/ItemStack;)I";
-	
+
 	private final String NEW_FURNACE_GEN_METHOD = "getFuelBurnFurnace";
 	private final String NEW_SURVIVALIST_GEN_METHOD = "getFuelBurnSurvivalist";
 
@@ -34,12 +39,14 @@ public class CoreTransformer implements IClassTransformer
 			return transformFurnaceClass(basicClass, NEW_FURNACE_GEN_METHOD);
 		else if (name.compareTo(SURVIVALIST_GEN_CLASS) == 0)
 			return transformFurnaceClass(basicClass, NEW_SURVIVALIST_GEN_METHOD);
+		else if (name.compareTo(KEYSTONE_CONTAINER_CLASS) == 0)
+			return transformKeystoneContainer(basicClass);
 		return basicClass;
 	}
 
 	private byte[] transformFurnaceClass(byte[] basicClass, String newMethod)
 	{
-		System.out.println("TPPITweaks transforming ExU Generator to method " + newMethod);
+		TPPITweaks.logger.info("Transforming ExU Generator to method " + newMethod);
 		ClassNode classNode = new ClassNode();
 		ClassReader classReader = new ClassReader(basicClass);
 		classReader.accept(classNode, 0);
@@ -59,10 +66,10 @@ public class CoreTransformer implements IClassTransformer
 						LabelNode jumpLabel = new LabelNode(new Label());
 
 						InsnList toInject = new InsnList();
-						
-						toInject.add(new VarInsnNode(Opcodes.ALOAD, 1));
 
-						toInject.add(new MethodInsnNode(Opcodes.INVOKESTATIC, "tppitweaks/core/CoreMethods", newMethod, FURNACE_GEN_METHOD_DESC));
+						toInject.add(new VarInsnNode(ALOAD, 1));
+
+						toInject.add(new MethodInsnNode(INVOKESTATIC, "tppitweaks/core/CoreMethods", newMethod, FURNACE_GEN_METHOD_DESC));
 
 						toInject.add(jumpLabel);
 						toInject.add(lmm1Node);
@@ -74,10 +81,37 @@ public class CoreTransformer implements IClassTransformer
 				}
 			}
 		}
-		
+
 		ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
 		classNode.accept(cw);
 
+		return cw.toByteArray();
+	}
+
+	private byte[] transformKeystoneContainer(byte[] basicClass)
+	{
+		TPPITweaks.logger.info("Transforming AM2 keystone container...");
+		ClassNode classNode = new ClassNode();
+		ClassReader classReader = new ClassReader(basicClass);
+		classReader.accept(classNode, 0);
+
+		Iterator<MethodNode> methods = classNode.methods.iterator();
+		while (methods.hasNext())
+		{
+			MethodNode m = methods.next();
+			if (m.name.equals("transferStackInSlot"))
+			{
+				m.instructions.clear();
+
+				m.instructions.add(new InsnNode(ACONST_NULL)); // forget it
+				m.instructions.add(new InsnNode(ARETURN));
+
+				break;
+			}
+		}
+
+		ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
+		classNode.accept(cw);
 		return cw.toByteArray();
 	}
 }
