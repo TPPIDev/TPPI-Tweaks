aba5e2468ae482e765009d5454cbfe5a161c6f25
tterrag1098 <tterrag1098@gmail.com>
2014-10-03 21:21:26 -0400

    Update/disable MPSA, update OpenBlocks

--- src/main/java/tppitweaks/recipetweaks/modTweaks/OpenBlocksTweaks.java
+++ src/main/java/tppitweaks/recipetweaks/modTweaks/OpenBlocksTweaks.java
@@ -2,13 +2,13 @@ package tppitweaks.recipetweaks.modTweaks;
 
 import java.util.Iterator;
 
-import net.minecraft.block.Block;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.monster.EntityPigZombie;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.ForgeSubscribe;
 import net.minecraftforge.event.entity.living.LivingDropsEvent;
 import net.minecraftforge.oredict.ShapedOreRecipe;
 import openblocks.OpenBlocks;
@@ -17,6 +17,7 @@ import tterrag.rtc.RecipeAddition;
 import tterrag.rtc.RecipeRemoval;
 import tterrag.rtc.TweakingRegistry;
 import tterrag.rtc.TweakingRegistry.TweakingAction;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
 
 public class OpenBlocksTweaks
@@ -27,10 +28,10 @@ public class OpenBlocksTweaks
 		if (ConfigurationHandler.eloraamBreakersAndDeployers)
 		{
 			if (openblocks.OpenBlocks.Blocks.blockBreaker != null)
-				TweakingRegistry.markItemForRecipeRemoval(((Block) openblocks.OpenBlocks.Blocks.blockBreaker).blockID, -1, TweakingAction.CHANGED, "Recipe changed to bring back", "RP2-like recipes");
+				TweakingRegistry.markItemForRecipeRemoval(openblocks.OpenBlocks.Blocks.blockBreaker, -1, TweakingAction.CHANGED, "Recipe changed to bring back", "RP2-like recipes");
 
 			if (openblocks.OpenBlocks.Blocks.blockPlacer != null)
-				TweakingRegistry.markItemForRecipeRemoval(((Block) openblocks.OpenBlocks.Blocks.blockPlacer).blockID, -1, TweakingAction.CHANGED, "Recipe changed to bring back", "RP2-like recipes");
+				TweakingRegistry.markItemForRecipeRemoval(openblocks.OpenBlocks.Blocks.blockPlacer, -1, TweakingAction.CHANGED, "Recipe changed to bring back", "RP2-like recipes");
 		}
 
 		if (ConfigurationHandler.disablePigmenTrophyDrop)
@@ -46,15 +47,15 @@ public class OpenBlocksTweaks
 		{
 			if (openblocks.OpenBlocks.Blocks.blockBreaker != null)
 				GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack((openblocks.OpenBlocks.Blocks.blockBreaker), 1), new Object[] { "CAC", "CPC", "CRC", 'C', "cobblestone", 'A',
-						Item.pickaxeIron, 'P', Block.pistonBase, 'R', Item.redstone }));
+						Items.iron_pickaxe, 'P', Blocks.piston, 'R', Items.redstone }));
 
 			if (openblocks.OpenBlocks.Blocks.blockPlacer != null)
-				GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack((openblocks.OpenBlocks.Blocks.blockPlacer), 1), new Object[] { "CAC", "CPC", "CRC", 'C', "cobblestone", 'A', Block.chest, 'P',
-						Block.pistonBase, 'R', Item.redstone }));
+				GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack((openblocks.OpenBlocks.Blocks.blockPlacer), 1), new Object[] { "CAC", "CPC", "CRC", 'C', "cobblestone", 'A', Blocks.chest, 'P',
+						Blocks.piston, 'R', Items.redstone }));
 		}
 	}
 
-	@ForgeSubscribe
+	@SubscribeEvent
 	public void onLivingDrop(LivingDropsEvent event)
 	{
 		if (event.entityLiving instanceof EntityPigZombie)
@@ -63,7 +64,7 @@ public class OpenBlocksTweaks
 			while (iter.hasNext())
 			{
 				ItemStack stack = iter.next().getEntityItem();
-				if (stack != null && stack.getItem() == Item.itemsList[((Block) OpenBlocks.Blocks.trophy).blockID])
+				if (stack != null && stack.getItem() == Item.getItemFromBlock(OpenBlocks.Blocks.trophy))
 				{
 					iter.remove();
 				}
