8876ea19c58b384bdf4b5fd36805b7ebf0836da1
tterrag1098 <tterrag1098@gmail.com>
2014-10-03 22:10:48 -0400

    Update/remove projectred, update Railcraft and SFM. That's all folks.

--- src/main/java/tppitweaks/recipetweaks/modTweaks/PRTweaks.java
+++ src/main/java/tppitweaks/recipetweaks/modTweaks/PRTweaks.java
@@ -1,7 +1,5 @@
 package tppitweaks.recipetweaks.modTweaks;
 
-import mrtjp.projectred.exploration.BlockSpecialStone;
-import net.minecraftforge.oredict.OreDictionary;
 import tterrag.rtc.RecipeAddition;
 
 public class PRTweaks
@@ -9,6 +7,6 @@ public class PRTweaks
 	@RecipeAddition(requiredModids="ProjRed|Core")
 	public static void registerMarble()
 	{
-		OreDictionary.registerOre("blockMarble", BlockSpecialStone.EnumSpecialStone.MARBLE.getItemStack());
+//		OreDictionary.registerOre("blockMarble", BlockSpecialStone.EnumSpecialStone.MARBLE.getItemStack());
 	}
 }
