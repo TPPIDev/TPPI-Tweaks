4dd89280d228546b1a191e6d6f795f10c1725635
tterrag1098 <tterrag1098@gmail.com>
2014-10-04 21:00:34 -0400

    Fix AE2 recipe

--- src/main/java/tppitweaks/item/ModItems.java
+++ src/main/java/tppitweaks/item/ModItems.java
@@ -2,6 +2,7 @@ package tppitweaks.item;
 
 import net.minecraft.item.ItemStack;
 import tppitweaks.config.ConfigurationHandler;
+import appeng.core.Api;
 import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.registry.GameRegistry;
 
@@ -19,18 +20,16 @@ public class ModItems
     {
         if (Loader.isModLoaded("AppliedEnergistics") && Loader.isModLoaded("StevesFactoryManager") && ConfigurationHandler.tweakSFM)
         {
-            // FIXME AppEng recipes
             /* @formatter:off */
-//			GameRegistry.addRecipe(new ItemStack(tppiMaterial.itemID, 1, 1),
-//		            new Object[] {
-//									"CSC",
-//									"SPS",
-//									"CSC",
-//		                                                    
-//									'C', Materials.matProcessorAdvanced.copy(),
-//									'P', Materials.matSilicon.copy(),
-//									'S', Materials.matFluxDust.copy(),
-//					});
+			GameRegistry.addRecipe(new ItemStack(tppiMaterial, 1, 1),
+			        "CSC",
+					"SPS",
+					"CSC",
+		                                                    
+					'C', Api.instance.materials().materialCalcProcessor.stack(1),
+					'P', Api.instance.materials().materialSilicon.stack(1),
+					'S', Api.instance.materials().materialFluixDust.stack(1)
+			);
 			/* @formatter:on */
 			
             GameRegistry.addSmelting(new ItemStack(tppiMaterial, 1, 1), new ItemStack(tppiMaterial), 0.1f);
