fe84211c06e53cd7a4cc76d455db8de4b750829e
FakoTheGreat <jeremyfako@gmail.com>
2015-02-22 01:10:16 -0700

    Hotfix for Rusty Ladder & Eln Wire conflict
    Changed Rusty Ladder recipe to double output, but takes six ingots per
    craft.

--- src/main/java/tppitweaks/recipetweaks/modTweaks/MalisisTweaks.java
+++ src/main/java/tppitweaks/recipetweaks/modTweaks/MalisisTweaks.java
@@ -1,6 +1,8 @@
 package tppitweaks.recipetweaks.modTweaks;
 
 import net.malisis.doors.MalisisDoors;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.oredict.ShapedOreRecipe;
 import tterrag.rtc.RecipeAddition;
 import tterrag.rtc.RecipeRemoval;
@@ -14,11 +16,19 @@ public class MalisisTweaks
     public static void removeRecipes()
     {
         TweakingRegistry.markItemForRecipeRemoval(MalisisDoors.Items.curtainsItem, 0, TweakingAction.CHANGED, "Recipe changed to force red wool", "for compat with Extra Utils");
+        
+        TweakingRegistry.markItemForRecipeRemoval(GameRegistry.findItem("malisisdoors", "rustyLadder"), 0, TweakingAction.CHANGED, "Recipe change to remove conflict", "with Electrical Age wire.");
     }
 
     @RecipeAddition(requiredModids = "malisisdoors")
     public static void addRecipes()
     {
         GameRegistry.addRecipe(new ShapedOreRecipe(MalisisDoors.Items.curtainsItem, "xx", "xx", "xx", 'x', "blockWoolRed"));
+        
+        GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(MalisisDoors.Items.curtainsItem, 2, 0), 
+        		"xxx", 
+        		"   ",
+        		"xxx",
+        		'x', "ingotIron"));
     }
 }
