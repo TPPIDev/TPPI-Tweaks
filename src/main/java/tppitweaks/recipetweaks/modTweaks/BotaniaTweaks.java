8cbf9c60f32c5c81b87d61e469fdeb1731c283dd
FakoTheGreat <jeremyfako@gmail.com>
2015-02-21 22:47:02 -0700

    Cleaned up Smokey Quartz code.

--- src/main/java/tppitweaks/recipetweaks/modTweaks/BotaniaTweaks.java
+++ src/main/java/tppitweaks/recipetweaks/modTweaks/BotaniaTweaks.java
@@ -2,6 +2,7 @@ package tppitweaks.recipetweaks.modTweaks;
 
 import net.minecraftforge.oredict.ShapelessOreRecipe;
 import thaumic.tinkerer.common.ThaumicTinkerer;
+import thaumic.tinkerer.common.item.quartz.ItemDarkQuartz;
 import tterrag.rtc.RecipeAddition;
 import tterrag.rtc.TweakingRegistry;
 import tterrag.rtc.TweakingRegistry.TweakingAction;
@@ -13,10 +14,10 @@ public class BotaniaTweaks {
 	@RecipeAddition(requiredModids = {"Botania","ThaumicTinkerer"})
 	public static void addSmokeyQuartzConversion()
 	{
-		TweakingRegistry.addTweakedTooltip(ThaumicTinkerer.registry.getFirstItemFromClass(thaumic.tinkerer.common.item.quartz.ItemDarkQuartz.class), 0, TweakingAction.NOTE, "Added recipe to convert","between the two types.");
+		TweakingRegistry.addTweakedTooltip(ThaumicTinkerer.registry.getFirstItemFromClass(ItemDarkQuartz.class), 0, TweakingAction.NOTE, "Added recipe to convert","between the two types.");
 		TweakingRegistry.addTweakedTooltip(ModItems.quartz, 0, TweakingAction.NOTE, "Added recipe to convert","between the two types.");
 		
-		GameRegistry.addRecipe(new ShapelessOreRecipe(ModItems.quartz, ThaumicTinkerer.registry.getFirstItemFromClass(thaumic.tinkerer.common.item.quartz.ItemDarkQuartz.class)));
-		GameRegistry.addRecipe(new ShapelessOreRecipe(ThaumicTinkerer.registry.getFirstItemFromClass(thaumic.tinkerer.common.item.quartz.ItemDarkQuartz.class), ModItems.quartz));
+		GameRegistry.addRecipe(new ShapelessOreRecipe(ModItems.quartz, ThaumicTinkerer.registry.getFirstItemFromClass(ItemDarkQuartz.class)));
+		GameRegistry.addRecipe(new ShapelessOreRecipe(ThaumicTinkerer.registry.getFirstItemFromClass(ItemDarkQuartz.class), ModItems.quartz));
 	}
 }
