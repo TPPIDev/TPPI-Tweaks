be40041392f9f979b1324e004e52b9f88f3a4597
TehNut <nick_ignoffo@yahoo.com>
2015-04-04 18:22:26 -0700

    Update Quarry recipe and remove Menagerie Tweaks
    Quarry now uses the Diamond Drill again, but replaces the Compressed
    Redstone Block with a BC Robot.

--- src/main/java/tppitweaks/recipetweaks/modTweaks/BuildcraftTweaks.java
+++ src/main/java/tppitweaks/recipetweaks/modTweaks/BuildcraftTweaks.java
@@ -1,6 +1,6 @@
 package tppitweaks.recipetweaks.modTweaks;
 
-import ic2.core.Ic2Items;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.oredict.OreDictionary;
 import net.minecraftforge.oredict.ShapedOreRecipe;
@@ -10,29 +10,32 @@ import tterrag.rtc.RecipeRemoval;
 import tterrag.rtc.TweakingRegistry;
 import tterrag.rtc.TweakingRegistry.TweakingAction;
 import cpw.mods.fml.common.registry.GameRegistry;
-import tppitweaks.block.ModBlocks;
 
 public class BuildcraftTweaks {
-	@RecipeRemoval(requiredModids={"IC2","BuildCraft|Factory"})
-	public static void init(){
-		if(ConfigurationHandler.harderQuarryRecipe){
-			TweakingRegistry.markItemForRecipeRemoval(GameRegistry.findItem("BuildCraft|Factory", "machineBlock"), 0, TweakingAction.CHANGED, "Replaced Diamond Pickaxe with Drill", "as temporary cost adjustment.");
-		}
-	}
-	
-	@RecipeAddition(requiredModids={"IC2","BuildCraft|Factory"})
-	public static void addRecipes(){
-		if(ConfigurationHandler.harderQuarryRecipe){
-			GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(GameRegistry.findItem("BuildCraft|Factory", "machineBlock")),
-					"IRI",
-					"GIG",
-					"DAD",
-					'I', "gearIron",
-					'R', new ItemStack(ModBlocks.tppiBlock),
-					'G', "gearGold",
-					'D', "gearDiamond",
-					'A', new ItemStack(Ic2Items.iridiumDrill.getItem(), 1, OreDictionary.WILDCARD_VALUE)
-					));
-		}
-	}
+    static Item machineBlock = GameRegistry.findItem("BuildCraft|Factory", "machineBlock");
+    static Item robot = GameRegistry.findItem("BuildCraft|Silicon", "robot");
+    static Item drillDiamond = GameRegistry.findItem("IC2", "itemToolDDrill");
+
+    @RecipeRemoval(requiredModids={"IC2", "BuildCraft|Factory", "BuildCraft|Silicon"})
+    public static void init(){
+        if(ConfigurationHandler.harderQuarryRecipe){
+            TweakingRegistry.markItemForRecipeRemoval(machineBlock, 0, TweakingAction.CHANGED, "Tweaked recipe to be", "more balanced with other", "forms of automated mining");
+        }
+    }
+
+    @RecipeAddition(requiredModids={"IC2","BuildCraft|Factory", "BuildCraft|Silicon"})
+    public static void addRecipes(){
+        if(ConfigurationHandler.harderQuarryRecipe) {
+            GameRegistry.addRecipe(new ShapedOreRecipe(machineBlock,
+                    "IRI",
+                    "GIG",
+                    "DAD",
+                    'I', "gearIron",
+                    'R', robot,
+                    'G', "gearGold",
+                    'D', "gearDiamond",
+                    'A', new ItemStack(drillDiamond, 1, OreDictionary.WILDCARD_VALUE)
+            ));
+        }
+    }
 }
